553921e5cd7c3b78110137848294af41ceeb11ed
Shayne999 <shaynejuniorn@gmail.com>
2024-06-26 17:15:47 +0200

    Complete

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,9 +3,7 @@
 """ Returns pascals triangle """
 
 def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
+    
     res = []
     if n > 0:
         for i in range(1, n + 1):
